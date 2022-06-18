b116171394e4baef1e12da3adab4704fb3b73013
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2022-06-18 13:05:52 +0200

    Added grpc07.

---
+++ java-samples/grpc-samples/grpc-07/grpc-07-server/src/main/java/io/protocol/grpc07/server/AverageServiceImpl.java
@@ -0,0 +1,44 @@
+package io.protocol.grpc07.server;
+
+import io.grpc.stub.StreamObserver;
+import io.protocol.grpc07.AverageRequest;
+import io.protocol.grpc07.AverageResponse;
+import io.protocol.grpc07.AverageServiceGrpc;
+import io.protocol.grpc07.AverageServiceProto;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AverageServiceImpl extends AverageServiceGrpc.AverageServiceImplBase {
+
+    @Override
+    public StreamObserver<AverageRequest> average(StreamObserver<AverageResponse> responseObserver) {
+        return new StreamObserver<AverageRequest>() {
+            int total=0;
+            int counter=0;
+            @Override
+            public void onNext(AverageRequest averageRequest) {
+                log.info("Received: {} ",averageRequest);
+                total+=averageRequest.getNumber();
+                counter++;
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                responseObserver.onError(throwable);
+
+            }
+
+            @Override
+            public void onCompleted() {
+                int average=total/counter;
+                log.info("Sending average : {} ",average);
+                //Sent average
+                responseObserver.onNext(AverageResponse.newBuilder().setAverage(average).build());
+
+                //Close connection
+                responseObserver.onCompleted();
+            }
+        };
+
+    }
+}
